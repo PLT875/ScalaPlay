a78766a5c98a7efcb9c3e5e47d7af9dd9268e9b3
Peter Tran <peter.tran@uclmail.net>
2014-12-27 11:46:03 +0000

    Update

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,19 +2,33 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+
 
 object Application extends Controller {
+
+  val form = Form(single("country" -> text))
 
   def index = Action {
     Ok(views.html.index("Play & Scala Example"))
   }
-  
+
   def world = Action {
-    Ok(views.html.world("World Database"))
+    Ok(views.html.world("Hello World"))
   }
-  
+
   def manual = Action {
     Ok(views.html.manual("Play Documentation"))
   }
 
+  def cities = Action {
+    Ok(views.html.helloworld.cities("List Cities by Country"))
+  }
+
+  def citiesSubmit = Action { implicit request =>
+    val (country) = form.bindFromRequest.get
+    Ok("%s".format(country))
+  }
 }
